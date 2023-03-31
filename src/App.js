77585c4e5a115ef4cadd852159530fdf37bebd42
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-04-01 00:13:54 +0100

    add content to sidebar container and change size of header icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
@@ -8,9 +9,12 @@ function App() {
       {/* Header */}
       <Header />
       {/* App body */}
+      <div className="app_body">
         {/* Sidebar */}
+        <Sidebar />
         {/* Feed */}
         {/* Widgets */}
+      </div>
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,29 @@
+import { Avatar } from '@mui/material'
+import './Sidebar.css'
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+        <div className="sidebar_top">
+            <img src="" alt="" />
+            <Avatar className="sidebar_avatar" />
+            <h2>Cameron Poole</h2>
+            <h4>cameron.edek.poole@gmail.com</h4>
+        </div>
+
+        <div className="sidebar_stats">
+            <div className="sidebar_stat">
+
+            </div>
+            <div className="sidebar_stat">
+                
+            </div>
+        </div>
+        <div className="sidebar_bottom">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar

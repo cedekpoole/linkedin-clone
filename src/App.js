95cdfd266786f29a5a8a2cdc583e4cabd83fe6d8
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-04-04 18:21:45 +0100

    keep user signed in on page reload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
 import Login from './Login';
-import { selectUser } from './features/userSlice';
-import { useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 import { auth } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth';
 
 function App() {
-  const user = useSelector(selectUser)
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    auth
-  }, [])
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(login({
+          email: user.email,
+          uid: user.uid,
+          displayName: user.displayName,
+          photoUrl: user.photoURL
+        }))
+        // if user is logged in
+      } else {
+        // if user is logged out
+        dispatch(logout())
+      }
+
+    })
+  }, [dispatch])
 
   return (
     <div className="app">
